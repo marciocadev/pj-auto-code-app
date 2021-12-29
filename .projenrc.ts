f48e0f7cf4f6f90d4ee2d10d40c3d07971f620af
Marcio Cruz de Almeida <marciocadev@gmail.com>
2021-12-29 08:09:33 -0300

    feat(init): first commit

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,14 +1,51 @@
-import { awscdk } from "projen";
+import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: "2.3.0",
-  defaultReleaseBranch: "main",
-  name: "pj-auto-code-app",
+  cdkVersion: '2.3.0',
+  defaultReleaseBranch: 'main',
+  name: 'pj-auto-code-app',
   projenrcTs: true,
+
+  // don't update snapshot when run jest
+  antitamper: false,
+  docgen: true,
+  eslint: true,
+  tsconfig: {
+    compilerOptions: {
+      lib: ['dom', 'es2019'],
+    },
+  },
+  // Major version number
+  /*
+    Everything before 1.0.0 is considered experimental and
+    breaking changes are only minor version bumps. The moment
+    ou feel comfortable you need to bump the version manually
+    to 1.0.0 and then the well-known versioning kicks in where
+    breaking changes bump the major version, features bump the
+    minor and fixes bump the patch version.
+
+    https://github.com/projen/projen/issues/982
+    The commit contains the following structural elements, to
+    communicate intent to the consumers of your library:
+    1. fix: a commit of the type fix patches a bug in your
+    codebase (this correlates with PATCH in semantic versioning).
+    2. feat: a commit of the type feat introduces a new feature to
+    the codebase (this correlates with MINOR in semantic versioning).
+    3. BREAKING CHANGE: a commit that has the text BREAKING CHANGE:
+    at the beginning of its optional body or footer section introduces
+    a breaking API change (correlating with MAJOR in semantic versioning).
+    A BREAKING CHANGE can be part of commits of any type.
+    4. Others: commit types other than fix: and feat: are allowed, for
+    example @commitlint/config-conventional (based on the Angular
+    convention) recommends chore:, docs:, style:, refactor:, perf:,
+    test:, and others.
+  */
+  // majorVersion: 1,
+  release: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  // packageName: undefined,  /* The 'name' in package.json. */
   // release: undefined,      /* Add release management to this project. */
 });
 project.synth();
