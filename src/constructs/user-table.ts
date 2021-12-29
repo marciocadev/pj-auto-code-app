803ae7dd495a870ae3e72577836c1af73dd8e7bb
Marcio Cruz de Almeida <marciocadev@gmail.com>
2021-12-29 11:42:47 -0300

    feat(refactor): refactoring

--- src/constructs/user-table.ts
+++ src/constructs/user-table.ts
@@ -4,7 +4,24 @@ import { Table, AttributeType } from 'aws-cdk-lib/aws-dynamodb';
 import { Function } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
+export enum GrantType {
+  Read = 1,
+  Write = 2,
+  ReadWrite = 3,
+}
+
+/**
+* A Cloudformation 'AWS::DynamoDB::Table' for **User** data
+*
+* @cloudformationResource AWS::DynamoDB::Table
+*/
 export class UserTable extends Table {
+  /**
+  * Create a new UserTable
+  *
+  * @param scope - scope in which this resource is defined
+  * @param id    - scoped id of the resource
+  */
   constructor(scope: Construct, id: string) {
     super(scope, id, {
       partitionKey: {
@@ -19,15 +36,22 @@ export class UserTable extends Table {
     });
   }
 
-  public bind(handler: Function, grantType: string) {
+  /**
+  * Binding a table grant type to a handler
+  *
+  * @stability stable
+  * @param handler
+  * @param grantType
+  */
+  public bind(handler: Function, grantType: GrantType) {
     handler.addEnvironment('USER_TABLE_NAME', this.tableName);
-    if (grantType.toLowerCase() === 'write') {
+    if (grantType === GrantType.Write) {
       this.grantWriteData(handler);
     }
-    if (grantType.toLowerCase() === 'read') {
+    if (grantType === GrantType.Read) {
       this.grantReadData(handler);
     }
-    if (grantType.toLowerCase() === 'readwrite') {
+    if (grantType === GrantType.ReadWrite) {
       this.grantReadWriteData(handler);
     }
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, Aspects, Stack, StackProps } from 'aws-cdk-lib';
+import { AwsSolutionsChecks } from 'cdk-nag';
 import { Construct } from 'constructs';
 import { UserTable } from './constructs/user-table';
 
+/**
+ * Basic stack
+ */
 export class PjAutoCodeAppStack extends Stack {
+  /**
+   * Create a new PjAutoCodeAppStack
+   *
+   * @param scope - scope in which this resource is defined
+   * @param id    - scoped id of the resource
+   * @param props - resource properties
+   */
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
@@ -20,5 +31,7 @@ const app = new App();
 
 new PjAutoCodeAppStack(app, 'my-stack-dev', { env: devEnv });
 // new MyStack(app, 'my-stack-prod', { env: prodEnv });
+
+Aspects.of(app).add(new AwsSolutionsChecks());
 
 app.synth();
